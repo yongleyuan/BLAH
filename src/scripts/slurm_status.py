c16de5d33a1b1aca7ba6fbe006ac858522c2fb1c
Brian Lin <brianhlin@gmail.com>
2020-10-14 18:08:47 -0500

    Handle memory values with decimal places

--- src/scripts/slurm_status.py
+++ src/scripts/slurm_status.py
@@ -369,7 +369,7 @@ def get_finished_job_stats(jobid, cluster):
                 else:
                     # The last value is not a letter (or unrecognized scaling factor), and is in bytes, convert to k
                     value = str(int(value) / 1024)
-                return_dict["ImageSize"] += int(value.strip('KMGTP')) * factor
+                return_dict["ImageSize"] += int(float(value.strip('KMGTP'))) * factor
             except:
                 log("Failed to parse memory usage for job id %s: %s" % (jobid, row["MaxRSS"]))
                 raise
